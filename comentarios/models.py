be44a3a097a022d97db18b929445a8c1b4b73979
Alejandro Luna <alejandro.luna@ciencias.unam.mx>
2015-11-16 00:21:08 -0600

    Layout base y cambio de modelo

--- comentarios/models.py
+++ comentarios/models.py
@@ -1,17 +1,22 @@
+from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from comercio.models import Comercio
 from usuarios.models import Usuario
+
 # Create your models here.
 class Comentario(models.Model):
 	comentario = models.TextField()
 	comercioId = models.ForeignKey(Comercio)
 	fecha = models.DateTimeField(auto_now_add = True)
-	usuarioId = models.ForeignKey(Usuario)
+	usuarioId = models.ForeignKey(User)
+	def __str__(self):
+		return self.comentario
 
 class Califica (models.Model):
 	califica = models.PositiveIntegerField(
 		validators=[MaxValueValidator(10), MinValueValidator(1)]
 		)
 	comercioId = models.ForeignKey(Comercio)
-	usuarioId = models.ForeignKey(Usuario)
+	usuarioId = models.ForeignKey(User)
+
