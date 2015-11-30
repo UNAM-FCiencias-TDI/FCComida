3a1868003a4119739a4e231ec92b37d365b6e505
Miguel Gaspar <magasparb@gmail.com>
2015-11-30 00:28:47 -0600

    Validaciones en formulario de registro

--- perfiles/models.py
+++ perfiles/models.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,8 +8,24 @@ class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
 
+    FACULTADES = (
+    	('Facultad de Arquitectura', 'Facultad de Arquitectura'),
+    	('Facultad de Derecho','Facultad de Derecho'),
+    	('Facultad de Ciencias','Facultad de Ciencias'),
+    	('Facultad de Ciencias Políticas y Sociales','Facultad de Ciencias Políticas y Sociales'),
+    	('Facultad de Contaduría y Administración','Facultad de Contaduría y Administración'),
+    	('Facultad de Economía','Facultad de Economía'),
+    	('Facultad de Filosofía y Letras','Facultad de Filosofía y Letras'),
+    	('Facultad de Ingeniería','Facultad de Ingeniería'),
+    	('Facultad de Medicina','Facultad de Medicina'),
+    	('Facultad de Química','Facultad de Química'),
+    	('Facultad de Odontología','Facultad de Odontología'),
+    	('Facultad de Psicología','Facultad de Psicología'),
+    	('Facultad de Veterinaria y Zootecnia','Facultad de Veterinaria y Zootecnia'),
+    )
+
     # The additional attributes we wish to include.
-    facultad = models.CharField(max_length=200, blank=True)
+    facultad = models.CharField(max_length=200, choices=FACULTADES)
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     # Override the __unicode__() method to return out something meaningful!
