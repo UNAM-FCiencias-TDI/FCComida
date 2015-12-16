20313157ec888cc18567796b2fe9aa6dda953127
Alejandro Luna <alejandro.luna@ciencias.unam.mx>
2015-12-16 10:32:23 -0600

    Correccion de errores y pagina de inicio

--- FCComida/views.py
+++ FCComida/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from comercio.models import Comercio
 def inicio(request):
-    return render(request, 'home.html')
+	#Lista de los comercios a mostrar en el index (top 10), temporalmente se ordenan por el nombre
+	lista_comercios = Comercio.objects.order_by('nombre')[:10]
+	#Diccionario para iterar en el index
+	comercios_dic = {'Comercios': lista_comercios}
+	return render(request, 'home.html', comercios_dic)
 def perfiles(request):
     return render(request, 'home.html')
 
--- comercio/views.py
+++ comercio/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from .forms import ComercioForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-# Creamos la vista para registrar un comercio
+# Creamos la vista para registrar un comercio, solo usuarios con sesion iniciada
+#Pueden registrar comercios, hay que manejar el error para cuando no lo estan
+@login_required
 def registro(request):
 	# recibimos los datos por post
 	if request.method == 'POST':
@@ -15,21 +17,21 @@ def registro(request):
 			# Vemos si se adjunto alguna foto del comercio
 			if 'foto' in request.FILES:
 				form.foto = request.FILES['foto']
-
-
-
-		# Guardamos el comercio en la base de datos
-		form.save()
-
-		return redirect('comercio.views.exito')
-
+			# Guardamos el comercio en la base de datos
+			form.save()
+			#Esta linea y la anterior estaban mal identadas
+			#Por eso moria con datos invalidos
+			return redirect('comercio.views.exito')
+		#Imprime los errores en el formulario
+		else: 
+			print form.errors
 
 	else: 
 		form = ComercioForm()
 
 	return render(request,'comercio_registro.html', {'form': form})
 
-# Creamos una vista de exito a alguna accion
+# Creamos una vista de exito a alguna accion, este HTML no existe
 def exito(request):
 	return render(request, 'exito.html')
 
