691699697188b000d676833293209d21916f00c3
moi5 <el_moi@ciencias.unam.mx>
2015-12-17 02:36:27 -0600

    Create val.js

---
+++ templates/js/val.js
@@ -0,0 +1,57 @@
+(function($,W,D)
+{
+    var VALIDA = {};
+
+    VALIDA.UTIL =
+    {
+        validacion: function()
+        {
+
+            $("#user_form").validate({
+                rules: {
+                    usuario: {
+                        required: true,
+                        minlength: 3
+                    },
+
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true,
+                        minlength: 5
+                    },
+                    password2: {
+                        required: true,
+                        minlength: 5
+                    }
+                },
+                messages: {
+                    usuario: {
+                        required: "pon tu usuario",
+                        minlength: "almenos de 3 caracteres"
+                    },
+                    password: {
+                        required: "pon tu password",
+                        minlength: "almenos de 5 caracteres"
+                    },
+                    password2: {
+                        required: "repite tu password",
+                        minlength: "almenos de 5 caracteres"
+                    },
+                    email: "pon un email valido"
+
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        }
+    }
+
+    $(D).ready(function($) {
+        VALIDA.UTIL.validacion();
+    });
+
+})(jQuery, window, document);
