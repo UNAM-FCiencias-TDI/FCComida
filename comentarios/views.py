1a7142a59aab10c0432a8b664ee8d7df1fc37b9c
Alejandro Luna <alejandro.luna@ciencias.unam.mx>
2015-12-23 17:21:26 -0600

    Se permite califiicar comercios

--- comentarios/views.py
+++ comentarios/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from comentarios.models import Comentario
+from comentarios.models import Califica
 from comercio.models import Comercio
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.db.models import Avg
 # Create your views here.
+@login_required
 def Comentarios(request):
 	if request.method == 'POST':
 		#Tomamos los datos de los campos del formulario por su nombre
@@ -19,4 +23,27 @@ def Comentarios(request):
 		return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 	else:
 		#Con get lo mando a otro lado, no se si sea lo mejor
+		return render(request, 'login.html', {})
+#Analogo al anterior
+@login_required
+def Calificaciones(request):
+	if request.method == 'POST':
+		comercio = request.POST.get('comercioId')
+		califica = request.POST.get('rating')
+		usuarioId = request.user
+		comercioId = Comercio.objects.get(id=comercio)
+		calif = Califica.objects.create(califica=califica,comercioId=comercioId,usuarioId=usuarioId)
+		calif.save()
+		#obtenemos el conjunto de calificaciones del comericio
+		prom = Califica.objects.filter(comercioId=comercioId)
+		#calculamos el promedio y lo guardamos como entero
+		promedio = int (round (prom.aggregate(Avg('califica'))['califica__avg']))
+		#Obtenemos el comercio calificado
+		detalles = Comercio.objects.get(pk=comercio)
+		#Guardamos el promedio en el comercio
+		detalles.calificacion = promedio
+		#Guardamos el comercio
+		detalles.save()
+		return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+	else:
 		return render(request, 'login.html', {})
